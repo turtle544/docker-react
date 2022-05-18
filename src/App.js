8dbd262be399a5bc75d84dfdf9dd689d2bb8a89b
turtle544 <turtle.hermit544@gmail.com>
2022-05-18 15:59:32 +0200

    files upload

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>
         </p>
         <a
           className="App-link"
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          co tam elo
         </a>
       </header>
     </div>
